face917dffdd17bfd68b772045ff3051d7c886d4
Richard Breslin <18ribreslin@gmail.com>
2022-07-28 22:48:28 -0400

    update imports

--- app.js
+++ app.js
@@ -15,45 +15,55 @@
 // ------------------------------------------------
 
 // Create an empty scene
-var scene = new THREE.Scene();
 
-// Create a basic perspective camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 4;
+function Index() {
+  var scene = new THREE.Scene();
 
-// Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({ antialias: true });
+  // Create a basic perspective camera
+  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 4;
 
-// Configure renderer clear color
-renderer.setClearColor("#000000");
+  // var camera2 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // camera.position.z = 4;
 
-// Configure renderer size
-renderer.setSize(window.innerWidth, window.innerHeight);
+  // Create a renderer with Antialiasing
+  var renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// Append Renderer to DOM
-document.body.appendChild(renderer.domElement);
+  // Configure renderer clear color
+  renderer.setClearColor("#000000");
 
-// ------------------------------------------------
-// FUN STARTS HERE
-// ------------------------------------------------
+  // const controls = new OrbitControls(camera2, renderer.domElement);
 
-// Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: "#433F81" });
-var cube = new THREE.Mesh(geometry, material);
+  // Configure renderer size
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Add cube to Scene
-scene.add(cube);
+  // Append Renderer to DOM
+  document.body.appendChild(renderer.domElement);
 
-// Render Loop
-var render = function () {
-  requestAnimationFrame(render);
+  // ------------------------------------------------
+  // FUN STARTS HERE
+  // ------------------------------------------------
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // Create a Cube Mesh with basic material
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var material = new THREE.MeshBasicMaterial({ color: "#433F81" });
+  var cube = new THREE.Mesh(geometry, material);
 
-  // Render the scene
-  renderer.render(scene, camera);
-};
+  // Add cube to Scene
+  scene.add(cube);
 
-render();
+  // Render Loop
+  var render = function () {
+    requestAnimationFrame(render);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // Render the scene
+    renderer.render(scene, camera);
+  };
+
+  render();
+}
+
+Index()
