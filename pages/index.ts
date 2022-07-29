5847a8118edee0292515dfa0a7e2af85c22ff714
Richard Breslin <18ribreslin@gmail.com>
2022-07-29 16:41:22 -0400

    moving background

--- pages/index.ts
+++ pages/index.ts
@@ -7,33 +7,133 @@ import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
 import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader.js";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 import { useEffect } from 'react';
+import { Fog } from 'three';
 
 
 function HomePage() {
+
+  const textureLoader: THREE.TextureLoader = new THREE.TextureLoader();
+  const gridTexture: THREE.Texture = textureLoader.load("/grid-6.png");
+  const heightTexture: THREE.Texture = textureLoader.load("/displacement-7.png");
+  const metalnessTexture: THREE.Texture = textureLoader.load("/metalness-2.png");
+
+
+
   //init scene class
   const scene: THREE.Scene = new THREE.Scene()
-
-  //init camera class
-  const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  const canvas: Element = document.querySelector("canvas.webgl");
 
   //init webgl renderer
   const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
 
+  //create a simple plane
+  const geometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(1, 2, 24, 24);
+
+  const planeProperties = {
+    displacementScale: 0.4,
+    metalness: 1,
+    roughness: 0.5,
+  };
+  const material = new THREE.MeshStandardMaterial({
+    map: gridTexture,
+    displacementMap: heightTexture,
+    displacementScale: planeProperties.displacementScale,
+    metalness: planeProperties.metalness,
+    metalnessMap: metalnessTexture,
+    roughness: planeProperties.roughness,
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  const plane2 = new THREE.Mesh(geometry, material);
+  plane.rotation.x = -Math.PI * 0.5;
+  plane2.rotation.x = -Math.PI * 0.5;
+
+  plane.position.y = 0.0;
+  plane.position.z = 0.15;
+  plane2.position.y = 0.0;
+  plane2.position.z = -1.85;
+  scene.add(plane);
+  scene.add(plane2);
+
+
+
+  const fog: Fog = new THREE.Fog("#000000", 1, 2.5);
+  scene.fog = fog;
+
+  // Lights
+  const ambientLight = new THREE.AmbientLight("#ffffff", 10);
+  scene.add(ambientLight);
+
+
+  const spotlight = new THREE.SpotLight(
+    "#d53c3d",
+    40,
+    25,
+    Math.PI * 0.1,
+    0.25
+  );
+  spotlight.position.set(0.5, 0.75, 2.1);
+  spotlight.target.position.x = -0.25;
+  spotlight.target.position.y = 0.25;
+  spotlight.target.position.z = 0.25;
+  scene.add(spotlight);
+  scene.add(spotlight.target);
+
+  const spotlight2 = new THREE.SpotLight(
+    "#d53c3d",
+    40,
+    25,
+    Math.PI * 0.1,
+    0.25
+  );
+  spotlight2.position.set(-0.5, 0.75, 2.1);
+  spotlight2.target.position.x = 0.25;
+  spotlight2.target.position.y = 0.25;
+  spotlight2.target.position.z = 0.25;
+  scene.add(spotlight2);
+  scene.add(spotlight2.target);
+
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.01,
+    20
+  );
+  camera.position.x = 0;
+  camera.position.y = 0.06;
+  camera.position.z = 1.1;
+  scene.add(camera);
+
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  const effectComposer = new EffectComposer(renderer);
+  effectComposer.setSize(sizes.width, sizes.height);
+  effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  const renderPass = new RenderPass(scene, camera);
+  effectComposer.addPass(renderPass);
+
+  const rgbShiftPass = new ShaderPass(RGBShiftShader);
+  rgbShiftPass.uniforms["amount"].value = 0.001;
+
+  effectComposer.addPass(rgbShiftPass);
+  const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
+  effectComposer.addPass(gammaCorrectionPass);
+
+
   //init control module
-  const controls = new OrbitControls(camera, renderer.domElement)
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
 
-  //create a simple wireframe box
-  const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
-  const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-
-  // add box to scene
-  const box: THREE.Mesh = new THREE.Mesh(geometry, material)
-  scene.add(box)
-
-  //init camera position
-  camera.position.z = 2;
 
   //event listener updates the canvas when the window is resized
   window.addEventListener('resize', onWindowResize, false)
@@ -42,14 +142,26 @@ function HomePage() {
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     renderer.render(scene, camera)
+    camera.aspect = sizes.width / sizes.height;
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    effectComposer.setSize(sizes.width, sizes.height);
+    effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
   }
 
+  const clock = new THREE.Clock();
   //animation loop
   var animate = function () {
     requestAnimationFrame(animate)
+    const elapsedTime = clock.getElapsedTime();
 
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
+    // Update plane position
+    plane.position.z = (elapsedTime * 0.15) % 2;
+    plane2.position.z = ((elapsedTime * 0.15) % 2) - 2;
+
+    effectComposer.render();
 
     controls.update()
 
@@ -61,7 +173,6 @@ function HomePage() {
   }
   render()
   animate();
-
 }
 
 
