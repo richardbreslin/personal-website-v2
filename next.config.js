f4001ea23c09486bd74b124a8e0e2bf47ddf40dc
Richard Breslin <18ribreslin@gmail.com>
2022-07-29 21:20:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@
  */
 const nextConfig = {
     /* config options here */
-    ignoreBuildErrors: true,
-
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 }
 
 module.exports = nextConfig
