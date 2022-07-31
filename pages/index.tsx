310d5baae3fbc664fb3d67d3f8098e8dd3d24585
Richard Breslin <18ribreslin@gmail.com>
2022-07-31 11:08:18 -0400

    convert HomePage to class component

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,189 +9,203 @@ import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPa
 import React from 'react';
 
 
-function HomePage() {
+type BackgroundProps = {};
+type BackgroundState = {};
 
-  const manager = new THREE.LoadingManager();
-  const loader = new THREE.TextureLoader(manager);
-  manager.onStart = function (url, itemsLoaded, itemsTotal) {
-    console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
-  };
-  manager.onProgress = function (url, itemsLoaded, itemsTotal) {
-    console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
-  };
-  manager.onError = function (url) {
-    console.log('There was an error loading ' + url);
+class Background3D extends React.Component<BackgroundProps, BackgroundState> {
+  state: BackgroundState = {
   };
 
-  const gridTexture: THREE.Texture = loader.load('/grid.png');
-  const heightTexture: THREE.Texture = loader.load("/displacement-7.png");
-  const metalnessTexture: THREE.Texture = loader.load("/metalness-2.png");
-  const bgTexture: THREE.Texture = loader.load('/background.jpg');
+  renderBackground() {
 
-
-  manager.onLoad = function () {
-    console.log('Loading complete!');
-    const scene = new THREE.Scene()
-
-
-    scene.background = bgTexture;
-
-
-    //init webgl renderer
-    const renderer = new THREE.WebGLRenderer({
-      alpha: true,
-    })
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-
-    const geometry = new THREE.PlaneGeometry(1, 2, 24, 24);
-
-
-    const material = new THREE.MeshStandardMaterial({
-      map: gridTexture,
-      displacementMap: heightTexture,
-      displacementScale: 0.4,
-      metalness: 1,
-      metalnessMap: metalnessTexture,
-      roughness: 0.5,
-    });
-
-    const plane = new THREE.Mesh(geometry, material);
-    const plane2 = new THREE.Mesh(geometry, material);
-    plane.rotation.x = -Math.PI * 0.5;
-    plane2.rotation.x = -Math.PI * 0.5;
-
-    plane.position.y = 0.0;
-    plane.position.z = 0.15;
-    plane2.position.y = 0.0;
-    plane2.position.z = -1.85;
-    scene.add(plane);
-    scene.add(plane2);
-
-
-
-    const fog = new THREE.Fog("#000000", 1, 2.5);
-    scene.fog = fog;
-
-    // Lights
-    const ambientLight = new THREE.AmbientLight("#ffffff", 10);
-    scene.add(ambientLight);
-
-
-    const spotlight = new THREE.SpotLight(
-      "#d53c3d",
-      50,
-      25,
-      Math.PI * 0.1,
-      0.25
-    );
-    spotlight.position.set(0.5, 0.75, 2.1);
-    spotlight.target.position.x = -0.25;
-    spotlight.target.position.y = 0.25;
-    spotlight.target.position.z = 0.25;
-    scene.add(spotlight);
-    scene.add(spotlight.target);
-
-    const spotlight2 = new THREE.SpotLight(
-      "#d53c3d",
-      50,
-      25,
-      Math.PI * 0.1,
-      0.25
-    );
-    spotlight2.position.set(-0.5, 0.75, 2.1);
-    spotlight2.target.position.set(.25, .25, .25);
-    scene.add(spotlight2);
-    scene.add(spotlight2.target);
-
-    const sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight,
+    const manager = new THREE.LoadingManager();
+    const loader = new THREE.TextureLoader(manager);
+    manager.onStart = function (url, itemsLoaded, itemsTotal) {
+      console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+    };
+    manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+      console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+    };
+    manager.onError = function (url) {
+      console.log('There was an error loading ' + url);
     };
 
-    // Base camera
-    const camera = new THREE.PerspectiveCamera(
-      60,
-      sizes.width / sizes.height,
-      0.01,
-      20
-    );
-
-    //camera xyz pos
-    camera.position.set(0, 0.06, 1.1);
-    scene.add(camera);
+    const gridTexture: THREE.Texture = loader.load('/grid.png');
+    const heightTexture: THREE.Texture = loader.load("/displacement-7.png");
+    const metalnessTexture: THREE.Texture = loader.load("/metalness-2.png");
+    const bgTexture: THREE.Texture = loader.load('/background.jpg');
 
 
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-    const effectComposer = new EffectComposer(renderer);
-    effectComposer.setSize(sizes.width, sizes.height);
-    effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-    const renderPass = new RenderPass(scene, camera);
-    effectComposer.addPass(renderPass);
-
-    const rgbShiftPass = new ShaderPass(RGBShiftShader);
-    rgbShiftPass.uniforms["amount"].value = 0.001;
-
-    effectComposer.addPass(rgbShiftPass);
-    const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
-    effectComposer.addPass(gammaCorrectionPass);
-
-    let bloomResolution = new THREE.Vector2(window.innerWidth, window.innerHeight)
-    const bloomPass = new UnrealBloomPass(bloomResolution, 1, 1, 1);
-
-    effectComposer.addPass(bloomPass);
+    manager.onLoad = function () {
+      console.log('Loading complete!');
+      const scene = new THREE.Scene()
 
 
-    //init control module
-    // const controls = new OrbitControls(camera, renderer.domElement);
-    // controls.enableDamping = true;
+      scene.background = bgTexture;
 
 
-    //event listener updates the canvas when the window is resized
-    window.addEventListener('resize', onWindowResize, false)
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight
-      camera.updateProjectionMatrix()
+      //init webgl renderer
+      const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+      })
       renderer.setSize(window.innerWidth, window.innerHeight)
-      renderer.render(scene, camera)
-      camera.aspect = sizes.width / sizes.height;
+      document.body.appendChild(renderer.domElement)
+
+      const geometry = new THREE.PlaneGeometry(1, 2, 24, 24);
+
+
+      const material = new THREE.MeshStandardMaterial({
+        map: gridTexture,
+        displacementMap: heightTexture,
+        displacementScale: 0.4,
+        metalness: 1,
+        metalnessMap: metalnessTexture,
+        roughness: 0.5,
+      });
+
+      const plane = new THREE.Mesh(geometry, material);
+      const plane2 = new THREE.Mesh(geometry, material);
+      plane.rotation.x = -Math.PI * 0.5;
+      plane2.rotation.x = -Math.PI * 0.5;
+
+      plane.position.y = 0.0;
+      plane.position.z = 0.15;
+      plane2.position.y = 0.0;
+      plane2.position.z = -1.85;
+      scene.add(plane);
+      scene.add(plane2);
+
+
+
+      const fog = new THREE.Fog("#000000", 1, 2.5);
+      scene.fog = fog;
+
+      // Lights
+      const ambientLight = new THREE.AmbientLight("#ffffff", 10);
+      scene.add(ambientLight);
+
+
+      const spotlight = new THREE.SpotLight(
+        "#d53c3d",
+        50,
+        25,
+        Math.PI * 0.1,
+        0.25
+      );
+      spotlight.position.set(0.5, 0.75, 2.1);
+      spotlight.target.position.x = -0.25;
+      spotlight.target.position.y = 0.25;
+      spotlight.target.position.z = 0.25;
+      scene.add(spotlight);
+      scene.add(spotlight.target);
+
+      const spotlight2 = new THREE.SpotLight(
+        "#d53c3d",
+        50,
+        25,
+        Math.PI * 0.1,
+        0.25
+      );
+      spotlight2.position.set(-0.5, 0.75, 2.1);
+      spotlight2.target.position.set(.25, .25, .25);
+      scene.add(spotlight2);
+      scene.add(spotlight2.target);
+
+      const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+
+      // Base camera
+      const camera = new THREE.PerspectiveCamera(
+        60,
+        sizes.width / sizes.height,
+        0.01,
+        20
+      );
+
+      //camera xyz pos
+      camera.position.set(0, 0.06, 1.1);
+      scene.add(camera);
+
+
       renderer.setSize(sizes.width, sizes.height);
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+      const effectComposer = new EffectComposer(renderer);
       effectComposer.setSize(sizes.width, sizes.height);
       effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    }
+      const renderPass = new RenderPass(scene, camera);
+      effectComposer.addPass(renderPass);
 
-    const clock = new THREE.Clock();
-    //animation loop
-    let animate = function (): void {
-      requestAnimationFrame(animate)
-      const elapsedTime = clock.getElapsedTime();
+      const rgbShiftPass = new ShaderPass(RGBShiftShader);
+      rgbShiftPass.uniforms["amount"].value = 0.001;
 
-      // Update plane position
-      let planeSpeed: number = 0.12;
-      plane.position.z = (elapsedTime * planeSpeed) % 2;
-      plane2.position.z = ((elapsedTime * planeSpeed) % 2) - 2;
+      effectComposer.addPass(rgbShiftPass);
+      const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
+      effectComposer.addPass(gammaCorrectionPass);
 
-      effectComposer.render();
+      let bloomResolution = new THREE.Vector2(window.innerWidth, window.innerHeight)
+      const bloomPass = new UnrealBloomPass(bloomResolution, 1, 1, 1);
 
-      // controls.update()
+      effectComposer.addPass(bloomPass);
 
-      renderer.render(scene, camera)
+
+      //init control module
+      // const controls = new OrbitControls(camera, renderer.domElement);
+      // controls.enableDamping = true;
+
+
+      //event listener updates the canvas when the window is resized
+      window.addEventListener('resize', onWindowResize, false)
+      function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        renderer.render(scene, camera)
+        camera.aspect = sizes.width / sizes.height;
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+        effectComposer.setSize(sizes.width, sizes.height);
+        effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+      }
+
+      const clock = new THREE.Clock();
+      //animation loop
+      let animate = function (): void {
+        requestAnimationFrame(animate)
+        const elapsedTime = clock.getElapsedTime();
+
+        // Update plane position
+        let planeSpeed: number = 0.12;
+        plane.position.z = (elapsedTime * planeSpeed) % 2;
+        plane2.position.z = ((elapsedTime * planeSpeed) % 2) - 2;
+
+        effectComposer.render();
+
+        // controls.update()
+
+        renderer.render(scene, camera)
+      };
+
+      function render(): void {
+        renderer.render(scene, camera)
+      }
+      render()
+      animate();
     };
-
-    function render(): void {
-      renderer.render(scene, camera)
-    }
-    render()
-    animate();
-  };
-
-  return (<div />)
+  }
+  componentDidMount(): void {
+    this.renderBackground();
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
 }
 
 function Content() {
@@ -230,12 +244,14 @@ export default class IndexPage extends React.Component<MyProps, MyState> {
   state: MyState = {
     ready: false,
   };
-  componentDidMount() {
-    HomePage();
-  }
+
   render() {
     return (
-      <Content></Content>
+      <div>
+        <Background3D></Background3D>
+        <Content></Content>
+      </div>
+
     );
   }
 }
